f81aca735fd12b3af71a276bea4e8324efac744d
ninoch <nazanin.alipourfard@gmail.com>
2015-04-25 22:34:38 +0430

    django started

---
+++ WebProject/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'WebProject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/$', TemplateView.as_view(template_name='home.html')),
+    url(r'^login/$', TemplateView.as_view(template_name='login.html')),
+    url(r'^signup/$', TemplateView.as_view(template_name='signup.html'), name='signup'),
+    url(r'^user_profile/$', TemplateView.as_view(template_name='user_profile.html')),
+
+)
